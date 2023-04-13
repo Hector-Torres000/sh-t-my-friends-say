bf6b9f49d095a614921168b6483480c5839f883a
Hector Torres <h.s.torres96@gmail.com>
2023-04-12 22:46:11 -0700

    test 2 for cyclic recommendation; change a few lines around

--- server.js
+++ server.js
@@ -4,46 +4,44 @@ const { MongoClient } = require('mongodb');
 const PORT = 8001;
 require('dotenv').config();
 
-const dbConnectionStr = process.env.DB_string;
-const client = new MongoClient(dbConnectionStr);
-
-let db;
-
-client
-  .connect((err) => {
-    if (err) {
-      console.error(err);
-      return false;
-    }
-    app.listen(process.env.PORT || PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .then((client) => {
-    db = client.db('friend-quote');
-  });
-
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const dbConnectionStr = process.env.DB_string;
+const client = new MongoClient(dbConnectionStr, { useUnifiedTopology: true });
+
 app.get('/', async (request, response) => {
-  let data = await client.db.collection('quote').find().toArray();
+  let data = await client
+    .db('friend-quote')
+    .collection('quote')
+    .find()
+    .toArray();
   return response.render('index.ejs', { quotes: data });
 });
 
-app.post('/addQuote', (request, response) => {
-  db.collection('quote')
-    .insertOne({
-      friend: request.body.friend,
-      quote: request.body.quote,
-    })
-    .then((result) => {
-      console.log('Shit Quote Added');
-      response.redirect('/');
-    })
-    .catch((error) => console.error(error));
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+    return false;
+  }
+  app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
+
+app.post('/addQuote', async (request, response) => {
+  const reqData = {
+    friend: request.body.friend,
+    quote: request.body.quote,
+  };
+  let data = await client
+    .db('friend-quote')
+    .collection('quote')
+    .insertOne(reqData);
+  console.log('Shit Quote Added');
+  return response.redirect('/');
 });
 
 // app.put('/addOneLike', (request, response) => {
